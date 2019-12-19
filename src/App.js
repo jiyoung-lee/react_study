93321681699e1512160180f0c1940f2a5d824625
jiyoung-lee <jno0717@naver.com>
2019-12-19 14:28:28 +0900

    create구현: onSubmit이벤트 추가/update 구현/delete 구현

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Subject from './component/Subject';
 import TOC from './component/TOC';
 import ReadContent from './component/ReadContent';
 import CreateContent from './component/CreateContent';
+import UpdateContent from './component/UpdateContent';
 import Control from './component/Control';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode: 'create',
+      mode: 'welcome',
       selected_content_id: 2,
       subject: { title: 'WEB', sub: 'world wide web!' },
       welcome: { title: 'Welcome', desc: 'Hello, React!!' },
@@ -21,27 +23,64 @@ class App extends Component {
       ]
     }
   }
-  render() {
+  getReadContent() {
+    var i = 0;
+    while (i < this.state.contents.length) {
+      var data = this.state.contents[i];
+      if (data.id === this.state.selected_content_id) {
+        return data;
+        break;
+      }
+      i = i + 1;
+    }
+  }
+  getContent() {
     var _title, _desc, _article = null;
     if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if (this.state.mode === 'read') {
-      var i = 0;
-      while (i < this.state.contents.length) {
-        var data = this.state.contents[i];
-        if (data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
     } else if (this.state.mode === 'create') {
-      _article = <CreateContent></CreateContent>
+      _article = <CreateContent onSubmit={function (_title, _desc) {
+        this.max_content_id = this.max_content_id + 1;
+        // var _contents = this.state.contents.concat(
+        //   { id: this.max_content_id, title: _title, desc: _desc }
+        // )
+        var _contents = Array.from(this.state.contents);
+        _contents.push({id: this.max_content_id, title: _title, desc: _desc});
+        this.setState({
+          contents: _contents,
+          mode:'read',
+          selected_content_id:this.max_content_id
+        });
+        console.log(_title, _desc)
+      }.bind(this)}></CreateContent>
+    } else if (this.state.mode === 'update') {
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc) {
+          var _contents = Array.from(this.state.contents);//Array.from()은 불변성
+          var i = 0;
+          while(i < _contents.length){
+            if(_contents[i].id === _id) {
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i = i + 1;
+          }
+          this.setState({
+            contents: _contents,
+            mode:'read'
+          });
+          console.log(_title, _desc)
+      }.bind(this)}></UpdateContent>
     }
+    return _article;
+  }
+  render() {
     return (
       <div className="App">
         <Subject
@@ -63,11 +102,33 @@ class App extends Component {
         >
         </TOC>
         <Control onChangeMode={function (_mode) {
+          if(_mode === 'delete'){
+            if(window.confirm('really?')){//참이면 삭제
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while(i < _contents.length){
+                if(_contents[i].id === this.state.selected_content_id){
+                  _contents.splice(i, 1);
+                  break;
+                }
+                i = i + 1;
+              }
+              this.setState({
+                mode:'welcome',
+                contents:_contents
+              });
+              alert('deleted!');
+            }
+          } else {
+            this.setState({
+              mode:_mode
+            });
+          }
           this.setState({
             mode: _mode
           });
         }.bind(this)}></Control>
-        {_article}
+        {this.getContent()}
       </div>
     );
   }
--- src/component/CreateContent.js
+++ src/component/CreateContent.js
@@ -8,7 +8,10 @@ class CreateContent extends Component {
         <form action="/crate_process" method="post"
           onSubmit={function(e){
             e.preventDefault();//지갑만들때는 참고 X
-            alert('Submit!!!');
+            this.props.onSubmit(
+              e.target.title.value,
+              e.target.desc.value
+            );
           }.bind(this)}
         >
           <p><input type="text" name="title" placeholder="title"></input></p>
